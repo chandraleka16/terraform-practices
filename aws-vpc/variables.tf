a25d5e826cd14405ed7ee4520ac37e91e6e78ccc
chandra217 <chandraammu1403@gmail.com>
2021-10-25 09:45:43 +0530

    add variables.tf

---
+++ aws-vpc/variables.tf
@@ -0,0 +1,23 @@
+variable "aws_access_key" {
+
+}
+variable "aws_secret_key" {
+
+}
+variable "private_key_path" {
+    default = "/Users/coda/Downloads/ac-new-key-pair.pem"
+}
+variable "key_name" {
+    default = "ac-new-key-pair"
+}
+variable "region" {
+    default= "us-east-2"
+}
+
+variable "network_address_space" {
+    default = "10.1.0.0/16"
+
+}
+variable "subnet1_address_space" {
+    default = "10.1.0.0/24"
+}
