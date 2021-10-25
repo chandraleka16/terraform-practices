7dd71644288d62a4807214db778f575837edb417
chandraleka16 <chandraleka233@gmail.com>
2021-10-25 11:21:55 +0530

    add s3 access and var tf

---
+++ aws-res-deployment/s3-access/variables.tf
@@ -0,0 +1,10 @@
+variable "aws_access_key" {
+    default = "AKIAQARPNQRPLBAYSWG6"
+}
+variable "aws_secret_key" {
+    default = "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"
+}
+
+variable "region" {
+    default = "us-east-2"
+}
